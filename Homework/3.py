576b806918101324da56a72f201fdcb5bdd1eec3
Bogdan <bogdan.bodescu19@gmail.com>
2026-01-05 19:56:22 +0200

    updated p3 and p4

--- Homework/3.py
+++ Homework/3.py
@@ -1,3 +1,14 @@
 # 3. Primește un nume de la tastatură și afișează-l cu litere mari.
-name = input("Introdu numele tau: ")
-print(name.upper())
+number_of_attempts = 0
+while number_of_attempts < 3:
+    try:
+        name = input("Introdu un nume: ")
+        if len(name) == 0:
+            print("Te rog introdu un nume nenul.")
+            number_of_attempts += 1
+        else:
+            print("Numele cu litere mari este:", name.upper())
+            break
+    except Exception as e:
+        print("A aparut o eroare: ", e)
+        number_of_attempts += 1
--- Homework/4.py
+++ Homework/4.py
@@ -1,3 +1,14 @@
 # 4. Afișează lungimea unui string introdus de la tastatură.
-text = input("Introdu un text: ")
-print(len(text))
+number_of_attempts = 0
+while number_of_attempts < 3:
+    try:
+        text = input("Introdu un text: ")
+        if len(text) == 0:
+            print("Te rog introdu un text nenul.")
+            number_of_attempts += 1
+        else:
+            print(f"Lungimea textului este: {len(text)}")
+            break
+    except Exception as e:
+        print("A aparut o eroare: ", e)
+        number_of_attempts += 1
