eaddfc1be9a1151934abf01d4a247c9b2c5bc347
Bogdan <bogdan.bodescu19@gmail.com>
2026-01-05 14:59:00 +0200

    Weather application 1st commit

---
+++ Weather project/weather.py
@@ -0,0 +1,6 @@
+import sys
+import requests
+from PyQT5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton ,QVBoxLayout 
+from Pyqt5.QtCore import Qt
+
+# 9990e6cf7897de350ea72fb70af9d018
