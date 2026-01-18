bc0f0b738a28846925bead44d3b011fa43d51b4c
Bogdan <bogdan.bodescu19@gmail.com>
2026-01-18 12:33:59 +0200

    adaugare stiva method la 19

--- Homework lists/19.py
+++ Homework lists/19.py
@@ -1,7 +1,7 @@
 # Sa se numere de cate ori apare un element intr-o lista incluzand si listele imbricate.
 # Exemplu: [1, 2, [3, 1, 4], 7, [1, 2, [1, 5]]] si elementul 1 -> apare de 4 ori
 
-def numara_element(lista, element_cautat):
+""" def numara_element(lista, element_cautat):
     count = 0
     for item in lista:
         if isinstance(item, list):
@@ -12,4 +12,22 @@ def numara_element(lista, element_cautat):
 lista = [1, 2, [3, 1, 4], 7, [1, 2, [1, 5]]]
 element_cautat = 1
 aparitii = numara_element(lista, element_cautat)
-print(f"Elementul {element_cautat} apare de {aparitii} ori in lista.")
+print(f"Elementul {element_cautat} apare de {aparitii} ori in lista.") """
+
+
+lista = [1, 2, [3, 1, 4], 7, [1, 2, [1, 5]]]
+element_cautat = 1
+stiva = [(lista, 0)]
+count = 0
+while stiva:
+    current_list, index = stiva.pop()
+    while index < len(current_list):
+        item = current_list[index]
+        index += 1
+        if isinstance(item,list):
+            stiva.append((current_list,index))
+            current_list = item
+            index = 0
+        elif item == element_cautat:
+            count += 1
+print(f"Elementul {element_cautat} apare de {count} ori in lista.")
